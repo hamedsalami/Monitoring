7d1267d2b8832d28b168e918aff3292701f0c3da
hamedsalami <hamedsalami1982@gmail.com>
2025-04-22 12:19:15 +0300

    Update grafana-installation.sh

--- grafana/grafana-installation.sh
+++ grafana/grafana-installation.sh
@@ -7,7 +7,7 @@ wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
-sudo apt-get install grafana
+sudo apt-get install grafana -y
 systemctl enable grafana-server
 systemctl start grafana-server
 #==========================================================================================
