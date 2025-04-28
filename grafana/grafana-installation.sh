34e471fcc5e70011fbbec1bcf71d3594ede398e3
hamedsalami <hamedsalami1982@gmail.com>
2025-04-28 16:04:50 +0300

    Update grafana-installation.sh

--- grafana/grafana-installation.sh
+++ grafana/grafana-installation.sh
@@ -1,6 +1,42 @@
-#==========================================================================================
-#Written by 'Hamed Salami' of 'www.hamedsalami.com'
-#==========================================================================================
+## Create User
+sudo adduser mikrotik
+sudo adduser mikrotik sudo
+su mikrotik
+cd
+## Install Prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v3.3.0/prometheus-3.3.0.linux-amd64.tar.gz
+tar xzf prometheus-3.3.0.linux-amd64.tar.gz
+sudo mkdir -p /data /etc/prometheus
+cd prometheus-3.3.0.linux-amd64
+sudo mv prometheus promtool /usr/local/bin/
+sudo mv prometheus.yml /etc/prometheus/prometheus.yml
+sudo chown -R mikrotik:mikrotik /etc/prometheus/ /data/
+cd
+## Install Node Exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+tar xzf node_exporter-1.9.1.linux-amd64.tar.gz
+mv node_exporter-1.9.1.linux-amd64/node_exporter /usr/local/bin/
+rm -rf node_exporter*
+cp ./node_exporter.service /etc/systemd/system
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+
+## Install Snmp Exporter
+wget https://github.com/prometheus/snmp_exporter/releases/download/v0.21.0/snmp_exporter-0.21.0.linux-amd64.tar.gz
+tar xzf snmp_exporter-0.21.0.linux-amd64.tar.gz
+cd snmp_exporter-0.21.0.linux-amd64
+sudo cp ./snmp_exporter /usr/local/bin/snmp_exporter
+sudo cp ./snmp.yml /usr/local/bin/snmp.yml
+cp ./snmp-exporter.service /etc/systemd/system
+systemctl daemon-reload
+sudo service snmp-exporter start
+sudo systemctl enable snmp-exporter
+##
+cp ./prometheus.service /etc/prometheus
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+sudo systemctl status prometheus
+##
 sudo apt-get install -y apt-transport-https software-properties-common wget
 sudo mkdir -p /etc/apt/keyrings/
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
@@ -10,4 +46,6 @@ sudo apt-get update
 sudo apt-get install grafana -y
 systemctl enable grafana-server
 systemctl start grafana-server
-#==========================================================================================
+
+	  
+
