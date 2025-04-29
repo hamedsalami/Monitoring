933d09a602ded6a5c844a92a1cb320cda8263af6
hamedsalami <hamedsalami1982@gmail.com>
2025-04-29 16:41:11 +0300

    Update grafana-installation.sh

--- grafana/grafana-installation.sh
+++ grafana/grafana-installation.sh
@@ -31,6 +31,7 @@ sudo systemctl enable snmp_exporter
 sudo cp ./prometheus.yml /etc/prometheus
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
+sudo systemctl restart prometheus
 ##
 sudo apt-get install -y apt-transport-https software-properties-common wget
 sudo mkdir -p /etc/apt/keyrings/
